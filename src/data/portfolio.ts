0a3a3c6abb917eed3a6d599555a1618b6cba25ad
Shachin-7 <shachinvp0506@gmail.com>
2026-08-19 20:13:56 +0530

    chore: update resume link

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -208,5 +208,5 @@ export const socialLinks = {
   github: "https://github.com/Shachin-7",
   linkedin: "https://www.linkedin.com/in/shachin-vp-859b26298",
   email: "shachinvp0506@gmail.com",
-  resume: "https://drive.google.com/file/d/1zrPJEXEJ5pHyukCFcqam7tTiSkABgHNo/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1gLka7ulBT3Q7W_eNuBG4mTUu1wfh-uWD/view?usp=sharing",
 };
