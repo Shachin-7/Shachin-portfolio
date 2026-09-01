a2927d67bfc438b32fa83df77d5fdffb2a8a0ef1
Shachin-7 <shachinvp0506@gmail.com>
2026-09-01 12:12:54 +0530

    docs: add JSDoc annotation to ThemeProvider component

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -3,6 +3,9 @@
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes";
 
+/**
+ * Application ThemeProvider wrapper integrating NextThemesProvider context.
+ */
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
